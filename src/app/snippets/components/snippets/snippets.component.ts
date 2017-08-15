2723c8319c650b3455ba2274859055f5db290790
Dani <daniru@gmail.com>
2017-08-15 08:17:03 +0100

    improve search

--- src/app/snippets/components/snippets/snippets.component.ts
+++ src/app/snippets/components/snippets/snippets.component.ts
@@ -12,10 +12,13 @@ import { Component, OnInit } from '@angular/core';
 export class SnippetsComponent implements OnInit {
 
   languages: Observable<string[]>;
-  snippets: Observable<Snippet[]>;
-  selected: string;
+  snippets: Observable<Snippet>;
 
+  selected: string;
   snippetToEdit: Snippet;
+
+  searchTerm = '';
+  language = 'IIS';
 
   constructor(private _snippetService: SnippetService) {}
 
@@ -25,13 +28,24 @@ export class SnippetsComponent implements OnInit {
       .do(x => console.log('on snippets', x))
       .map(x => _.uniq(_.map(x, (snippet) => snippet.language)))
       .do(x => this.selectLanguage('IIS'));
+
+    this.getSnippets();
   }
 
   selectLanguage(language: string) {
-    this.selected = language;
+    this.language = language;
+    this.getSnippets();
+  }
+
+  getSnippets() {
     this.snippets = this._snippetService
-      .getSnippets()
-      .map(x => _.filter(x, (snippet) => snippet.language === language));
+    .getSnippets()
+    .map(snippets => {
+      if (this.searchTerm && this.searchTerm.length > 2) {
+        return  _.filter(snippets, (snippet: Snippet) => snippet.title.toLowerCase().indexOf(this.searchTerm.toLowerCase()) >= 0);
+      }
+      return _.filter(snippets, (snippet: Snippet) => snippet.language === this.language);
+    });
   }
 
   editSnippet(snippet: Snippet) {
@@ -40,6 +54,11 @@ export class SnippetsComponent implements OnInit {
 
   finishEdit(snippet: Snippet) {
     this.snippetToEdit = null;
+  }
+
+  onChange(text: string) {
+    this.searchTerm = text;
+    this.getSnippets();
   }
 
 }
